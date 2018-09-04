7f9d570c152fa0c81df62423384b554f662413d5
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-09-04 13:39:07 -0500

    append div element working, color red, swap and delete buttons present

--- client.js
+++ client.js
@@ -4,5 +4,23 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    $('#generateButton').on('click', appendDiv);
+    $('swapButton').on('click', toggleColor);
+}
+
+totalClicks = 0;
+
+function appendDiv(){
+    console.log('in appendDiv');
+    $('body').append(`<div>
+    <p>` + totalClicks + `</p>
+    <button id="swapButton">Swap</button>
+    <button id="deleteButton">Delete</button>
+    </div>`);
+    totalClicks++;
+}
+
+function toggleColor(){
+    console.log('in toggle color');
     
 }
